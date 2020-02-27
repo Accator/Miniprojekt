3565dc2edf6b30e9a857fd4a09ba41c18f28f6f3
hakansam <46788076+hakansam@users.noreply.github.com>
2020-02-27 23:01:33 +0100

    Start

---
+++ projeckt.c
@@ -0,0 +1,35 @@
+
+
+
+//Storlekar valda för att underlätta "uppdaterandet" av skärmen.
+//Spelarens storlek är 8x8
+//Spelgrid som är 24 rader och 14 kolumner.
+
+
+//Spelet kör i en virutuell spelplan
+//Vi uppdaterar spelplanen hela tiden
+
+
+//Function: Spelet
+    //Function. Game interface tryck knapp för att starta.
+        //Function: Får in data med info om rörelse som uppdaterar "spelarens pos" temporära position.
+            //Function: som kollar om vi befinner oss innanför boxen i den virutella spelpalnen.
+            //Function: IF utanför, börja blinka. Else, pass.
+            //Function: Make temporära position, till current positon och uppdaterar spelplanen.
+            //Function: som uppdaterar OLED
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
