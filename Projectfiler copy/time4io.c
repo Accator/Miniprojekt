dbac9302cb647c9bb40426526d9c5ce488432435
hakansam <46788076+hakansam@users.noreply.github.com>
2020-02-29 23:07:21 +0100

    1

---
+++ Projectfiler copy/time4io.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <pic32mx.h>
+#include "mipslab.h"
+
+int getsw(void) {
+    return (PORTD >> 8) & 0xf;
+}
+
+int getbtns432(void) {
+	return (PORTD >> 5) & 0x7;
+}
+
+int getbtns1 (void){
+    return (PORTF >> 1) & 0x1;
+}
